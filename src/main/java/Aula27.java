c289749dcf34241abc23bc63537070a7f742c379
lenincaldeira <lenin.caldeira@gmail.com>
2022-07-27 19:37:32 -0300

    Aula 27 e 28 do curso de JAVA da Udemy

---
+++ src/main/java/Aula27.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Aula27 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        // COMO LER ATÉ A QUEBRA DE LINHA
+        int x;
+        String s1, s2, s3;
+
+        x = sc.nextInt();
+        sc.nextLine(); // QUEBRA DE LINHA PRA CONSUMIR A QUEBRA DE LINHA PENDENTE
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
+
+        System.out.println("DADOS DIGITADOS: ");
+        System.out.println(x);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+        sc.close();
+
+    }
+}
