03a3286a49779f5d1c66fe78db25fa61a38d3260
lenincaldeira <lenin.caldeira@gmail.com>
2022-08-03 13:49:23 -0300

    Aula 39 - Expressao condicional ternaria

---
+++ src/main/java/Aula40.java
@@ -0,0 +1,47 @@
+/*
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aula40 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double preco = 34.5;
+        double desconto;
+        if (preco < 20.0) {
+            desconto = preco * 0.1;
+        } else {
+            desconto = preco * 0.05;
+        }
+
+        System.out.println(desconto);
+
+        sc.close();
+
+    }
+}
+
+ */
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aula40 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double preco = 34.5;
+        double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+        System.out.println(desconto);
+
+        sc.close();
+
+    }
+}
