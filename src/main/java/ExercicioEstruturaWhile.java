8b46469e91fd374a62070e4a1ae298ec1ea6f18b
lenincaldeira <lenin.caldeira@gmail.com>
2022-08-04 20:38:11 -0300

    Aula 47- Exercicios usando estrutura While - questao 2

--- src/main/java/ExercicioEstruturaWhile.java
+++ src/main/java/ExercicioEstruturaWhile.java
@@ -1,3 +1,6 @@
+/*
+EXERCICIO 1:>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 import java.util.Scanner;
 
 public class ExercicioEstruturaWhile {
@@ -18,3 +21,40 @@ public class ExercicioEstruturaWhile {
         sc.close();
     }
 }
+
+EXERCICIO 2:>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+import java.util.Scanner;
+
+public class ExercicioEstruturaWhile {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        while (x != 0 && y!= 0) {
+                if (x > 0 && y > 0) {
+                    System.out.println("Primeiro");
+                }
+                else if (x > 0 && y < 0) {
+                    System.out.println("Segundo");
+                }
+                else if (x < 0 && y < 0) {
+                    System.out.println("Terceiro");
+                }
+                else {
+                    System.out.println("Quarto");
+                }
+                x = sc.nextInt();
+                y = sc.nextInt();
+        }
+
+        sc.close();
+    }
+}
+
+*/
+
