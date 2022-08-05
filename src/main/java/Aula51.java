c6a13067f9782f9683bfacb9e4463e7e94a9ce5e
lenincaldeira <lenin.caldeira@gmail.com>
2022-08-04 21:54:02 -0300

    Aula 51- Exercicio de teste de mesa com o FOR

---
+++ src/main/java/Aula51.java
@@ -0,0 +1,15 @@
+public class Aula51 {
+
+    public static void main(String[] args) {
+
+        int x = 0;
+        int y = 0;
+
+        int i;
+        for (i=0; i<x; i++) {
+            System.out.println(i + ",");
+            y = y + 5;
+            System.out.println(y);
+        }
+    }
+}
