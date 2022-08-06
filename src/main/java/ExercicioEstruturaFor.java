6a5655244c95f8667b6587cbb23aa00582d0db33
lenincaldeira <lenin.caldeira@gmail.com>
2022-08-05 22:46:56 -0300

    Aula 52- Exercicios estrutura FOR

---
+++ src/main/java/ExercicioEstruturaFor.java
@@ -0,0 +1,25 @@
+/*
+EXERCICIO 1>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+import java.util.Scanner;
+
+public class ExercicioEstruturaFor {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        for (int i=1; i<=x; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
+    }
+}
+
+EXERCICIO 2>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ */
