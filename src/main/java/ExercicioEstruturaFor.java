478ac9f3ef5c3214ae15efac486de071ae3cdf44
lenincaldeira <lenin.caldeira@gmail.com>
2022-08-05 22:58:36 -0300

    Aula 52- Exercicios estrutura FOR-Questao 2

--- src/main/java/ExercicioEstruturaFor.java
+++ src/main/java/ExercicioEstruturaFor.java
@@ -23,3 +23,32 @@ public class ExercicioEstruturaFor {
 
 EXERCICIO 2>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
  */
+import java.util.Scanner;
+
+public class ExercicioEstruturaFor {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int in = 0;
+        int out = 0;
+
+        for (int i=0; i<n; i++) {
+            int x = sc.nextInt();
+            if (x >= 10 && x <=20) {
+                in = in + 1;
+            }
+            else {
+                out = out = 1;
+            }
+        }
+
+        System.out.println(in + " in");
+        System.out.println(out + " out");
+
+        sc.close();
+    }
+}
