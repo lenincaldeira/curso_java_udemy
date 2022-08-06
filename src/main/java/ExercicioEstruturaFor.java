d794bac75a69682e4ad327f434677a91409aa00f
lenincaldeira <lenin.caldeira@gmail.com>
2022-08-06 10:03:29 -0300

    Aula 52- Exercicios estrutura FOR-Questao 4

--- src/main/java/ExercicioEstruturaFor.java
+++ src/main/java/ExercicioEstruturaFor.java
@@ -54,6 +54,34 @@ public class ExercicioEstruturaFor {
 }
 
 EXERCICIO 3>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExercicioEstruturaFor {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i=0; i<n; i++) {
+
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+            double c = sc.nextDouble();
+
+            double media = (a * 2 + b * 3 + c * 5)/10;
+
+            System.out.printf("%.1f%n", media);
+        }
+
+        sc.close();
+    }
+}
+
 EXERCICIO 4>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 EXERCICIO 5>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 EXERCICIO 6>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -94,20 +122,24 @@ public class ExercicioEstruturaFor {
 
         int n = sc.nextInt();
 
-        for (int i=0; i<n; i++) {
+        for (int  i=0; i<n; i++) {
 
-            double a = sc.nextDouble();
-            double b = sc.nextDouble();
-            double c = sc.nextDouble();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
 
-            double media = (a * 2 + b * 3 + c * 5)/10;
-
-            System.out.printf("%.1f%n", media);
+            if (y == 0){
+                System.out.println("Divisao Impossivel!");
+            }
+            else {
+                double div = (double) x / y;
+                System.out.printf("%.1f%n", div);
+            }
         }
 
         sc.close();
     }
 }
+
 
 
 
