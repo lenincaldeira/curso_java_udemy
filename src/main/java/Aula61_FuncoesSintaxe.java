000b9a7c528a64e6f944dba3f94e86d943fcf2b6
lenincaldeira <lenin.caldeira@gmail.com>
2022-08-08 19:05:57 -0300

    Aula 61 - Funcoes(Sintaxe) - exemplo 1

---
+++ src/main/java/Aula61_FuncoesSintaxe.java
@@ -0,0 +1,10 @@
+public class Aula61_FuncoesSintaxe {
+
+    public static void main(String[] args) {
+
+        double y = 25.0;
+        double x = Math.sqrt(y);
+
+        System.out.println(x);
+    }
+}
