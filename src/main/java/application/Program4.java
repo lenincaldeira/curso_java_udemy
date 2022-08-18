5f17f391dfa9d2766a93dea5256640ba9d330fba
lenincaldeira <lenin.caldeira@gmail.com>
2022-08-18 20:55:05 -0300

    Aula 70 - Exercicio 3

--- src/main/java/application/Program4.java
+++ src/main/java/application/Program4.java
@@ -14,7 +14,20 @@ public class Program4 {
 
             Student student = new Student();
 
-            
+            student.name = sc.nextLine();
+            student.grade1 = sc.nextDouble();
+            student.grade2 = sc.nextDouble();
+            student.grade3 = sc.nextDouble();
+
+            System.out.printf("FINAL GRADE: %.2f%n", student.finalGrade());
+
+            if (student.finalGrade() < 60.0) {
+                    System.out.println("FAILED");
+                    System.out.printf("MISSING %.2f POINTS%n", student.missingPoints());
+            }
+            else {
+                    System.out.println("PASS");
+            }
 
             sc.close();
         }
