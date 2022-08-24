49434e8d8e5bbccd1513263fb700a40b00e62fa8
lenincaldeira <lenin.caldeira@gmail.com>
2022-08-23 21:02:34 -0300

    Aula 72 e 73

---
+++ src/main/java/application/ProgramAula73.java
@@ -0,0 +1,11 @@
+package application;
+
+import entities.CurrencyConverter;
+
+public class ProgramAula73 {
+
+    public static void main(String[] args) {
+
+        CurrencyConverter curr = new CurrencyConverter();
+    }
+}
--- src/main/java/util/Calculator.java
+++ src/main/java/util/Calculator.java
@@ -2,14 +2,14 @@ package util;
 
 public class Calculator {
 
-    public final double PI = 3.14159; // Uma vez atribuido a palavra "final" ao "public static" a constante declarada nao pode ser alterada de forma nenhuma.
+    public static final double PI = 3.14159; // Uma vez atribuido a palavra "final" ao "public static" a constante declarada nao pode ser alterada de forma nenhuma.
     // padrao para nomes de constantes no JAVA serao em letras maiusculas. EX.: "public static final double NET_SALARY = 100.0"
 
-    public double circumference(double radius) {
+    public static double circumference(double radius) {
         return 2.0 * PI * radius;
     }
 
-    public double volume(double radius) {
+    public static double volume(double radius) {
         return 4.0 * PI * radius * radius * radius / 3.0;
     }
 }
