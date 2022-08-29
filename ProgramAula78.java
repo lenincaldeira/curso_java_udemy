ed675b92318da7d847c45f1442f1923b1f75826e
Lenin Caldeira <87790140+lenincaldeira@users.noreply.github.com>
2022-08-29 07:55:57 -0600

    Aula 78
    Sobrecarga

---
+++ ProgramAula78.java
@@ -0,0 +1,47 @@
+package application;
+
+import entities.Product;
+import entities.Product2;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramAula78 {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product2 p = new Product2();
+
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        double price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        int quantity = sc.nextInt();
+        Product2 product;
+        product = new Product2(name, price);
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+        quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+        sc.close();
+
+    }
+}
