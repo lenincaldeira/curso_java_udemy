42bf999a79a6ad36fce08ddc8479b0284a15b1a8
lenincaldeira <lenin.caldeira@gmail.com>
2022-08-30 11:53:37 -0300

    Aula 83 - Exercicios de fixacao

--- src/main/java/aplication/Program.java
+++
@@ -1,4 +0,0 @@
-package aplication;
-
-public class Program {
-}
--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -1,4 +1,13 @@
 package entities;
 
 public class Product {
+    public Product(String name, double price, int quantity) {
+
+    }
+
+    public void addProducts(int quantity) {
+    }
+
+    public void removeProducts(int quantity) {
+    }
 }
