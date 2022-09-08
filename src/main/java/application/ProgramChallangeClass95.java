bc2ab8dad533969e1b3357ffb2807b3aaeca2c74
lenincaldeira <lenin.caldeira@gmail.com>
2022-09-08 19:33:13 -0300

    Aula 95 - Desafio em java: cadastro de clientes em quartos de hotel

---
+++ src/main/java/application/ProgramChallangeClass95.java
@@ -0,0 +1,42 @@
+package application;
+
+import entities.Rent;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramChallangeClass95 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Rent[] vect = new Rent[10];
+
+        System.out.print("How many rooms will be rented? ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("\nRent #" + i + ": ");
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.nextLine();
+            System.out.print("Room: ");
+            int room = sc.nextInt();
+
+            vect[room] = new Rent(name, email);
+        }
+
+        System.out.println("\nBusy rooms: ");
+        for (int i = 0; i < 10; i++) {
+            if (vect[i] != null) {
+                System.out.println("Room "+ i + ": " + vect[i]);
+            }
+        }
+        
+        sc.close();
+    }
+}
