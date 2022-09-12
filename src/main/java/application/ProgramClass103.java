abb18f34a6dcf4ca3f9d13dfd16904e40a4daf85
lenincaldeira <lenin.caldeira@gmail.com>
2022-09-12 10:52:19 -0300

    Aula 103 - Exercicio de matrizes

--- src/main/java/application/ProgramClass103.java
+++ src/main/java/application/ProgramClass103.java
@@ -11,12 +11,27 @@ public class ProgramClass103 {
         int n = sc.nextInt();
         int [][] mat = new int [n][n];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < ,mat[i].length; j++) {
                 mat[i][j] = sc.nextInt();
             }
         }
 
+        System.out.println("main diagonal: ");
+        for (int i = 0; i < mat.length; i++) {
+            System.out.print(mat[i][i] + " ");
+        }
+        System.out.println();
+
+        int count = 0;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] < 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.println("Negative numbers = " + count);
 
         sc.close();
     }
