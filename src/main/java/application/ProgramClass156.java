96089a3ac9cc33ca066d8767c185d5d0b94f1dca
lenincaldeira <lenin.caldeira@gmail.com>
2022-10-06 21:19:25 -0300

    Aula 156 - Herança

---
+++ src/main/java/application/ProgramClass156.java
@@ -0,0 +1,9 @@
+package application;
+
+public class ProgramClass156 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/entities/BusinessAccountClass156.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class BusinessAccountClass156 extends AccountClass156 {
+
+    private Double loanLimit;
+
+    public BusinessAccountClass156() {
+        super();
+    }
+
+    public BusinessAccountClass156(Integer number, String holder, Double balance, Double loanLimit) {
+        super(number, holder, balance);
+        this.loanLimit = loanLimit;
+    }
+
+    public Double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(Double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+    public void loan(double amount) {
+        if (amount <= loanLimit) {
+            balance += amount - 10.0;
+        }
+    }
+}
